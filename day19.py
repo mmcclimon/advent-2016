e2cc73825dc963067d9ea08b551181fa7e038cdf
Michael McClimon <michael@mcclimon.org>
2023-12-16 22:03:03 -0500

    day 19: better, mathematical solutions

--- day19.py
+++ day19.py
@@ -1,40 +1,15 @@
 def main():
     n = 3005290
-    print("part 1:", Ring(n).go())
 
+    # solution to the Josephus problem from the Numberphile video, lol
+    print("part 1:", int(f"{n:b}"[1:] + "1", 2))
 
-class Node:
-    def __init__(self, n):
-        self.id = n
-        self.next: Node | None = None
-        # self.prev: Node | None = None
+    # Thes same thing, ish, but for powers of 3. (I needed some help here.)
+    i = 1
+    while i * 3 < n:
+        i *= 3
 
-
-class Ring:
-    def __init__(self, n):
-        self.n = n
-        self.root = Node(1)
-
-        prev = self.root
-        for elf in range(2, n + 1):
-            node = Node(elf)
-            prev.next = node
-            prev = node
-
-        prev.next = self.root
-
-    def go(self):
-        cur = self.root
-
-        while True:
-            if cur == cur.next:
-                break
-
-            to_steal = cur.next
-            cur.next = to_steal.next
-            cur = cur.next
-
-        return cur.id
+    print("part 2:", n - i)
 
 
 if __name__ == "__main__":
